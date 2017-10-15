208abf633834fc9ff501f1f88be3bcf68323d72f
Robert Pettersson <robert@bob-acken.com>
2017-10-15 00:07:04 -0700

    Adding a button as first component in storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -4,10 +4,10 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { Welcome } from '@storybook/react/demo';
+import {Button} from '../components'
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+  .add('Default', () => <Button onPress={action('Clicked')}>Default Button</Button>);

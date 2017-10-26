c8e6b558ac5a561f8599251eccc350497e8f89e9
Robert Pettersson <robert@bob-acken.com>
2017-10-25 22:02:39 -0700

    Adding react-router, and two empty layouts.

--- src/App.js
+++ src/App.js
@@ -1,52 +1,34 @@
 // @flow
 
-import React, { Component } from 'react';
-import { Motion, spring } from 'react-motion';
-import logo from './logo.svg';
-import './App.css';
-import ButtonPrimary from './components/Button/ButtonPrimary'
-import ButtonSecondary from './components/Button/ButtonSecondary'
+import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+import { Motion, spring } from 'react-motion'
+import {
+  Home,
+  About,
+} from './layouts'
+import logo from './logo.svg'
+import './App.css'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {open: false};
-  };
-
-  handleMouseDown = () => {
-    this.setState({open: !this.state.open});
-  };
-
-  handleTouchStart = (e) => {
-    e.preventDefault();
-    this.handleMouseDown();
-  };
-  render() {
+const App = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Bobby-UI</h1>
-        </header>
-        <p className="App-intro">
-          <ButtonPrimary onMouse={this.handleMouseDown} onTouch={this.handleTouchStart}>Primary</ButtonPrimary>
-          <ButtonSecondary>Secondary</ButtonSecondary>
-        </p>
-        <Motion style={{x: spring(this.state.open ? 400 : 0)}}>
-          {({x}) =>
-            // children is a callback which should accept the current value of
-            // `style`
-            <div className="demo0">
-              <div className="demo0-block" style={{
-                WebkitTransform: `translate3d(${x}px, 0, 0)`,
-                transform: `translate3d(${x}px, 0, 0)`,
-              }} />
-            </div>
-          }
-        </Motion>
-      </div>
-    );
-  }
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+            </ul>
+          </header>  
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+        </div>
+      </Router>
+    )
 }
 
 export default App;

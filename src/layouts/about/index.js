687ce45b3b67f58a9be9db341a33f465702f374b
Robert Pettersson <robert@bob-acken.com>
2018-03-06 12:38:31 -0800

    Adding basic about section with some easy hover effects

--- src/layouts/about/index.js
+++ src/layouts/about/index.js
@@ -1,16 +1,24 @@
-import React, { Component } from 'react'
+import React, { PureComponent } from 'react'
 
-export class About extends Component {
+export class About extends PureComponent {
+  addClass = () => {
+    var name = document.getElementById('image')
+    name.className += " anim";
+  }
+  removeClass = () => {
+    var name = document.getElementById('image')
+    name.classList.remove("anim");
+  }
   render() {
     return (
       <div className='about'>
-        <div className="top">
+        <div className="top" onMouseOver={() => this.addClass()} onMouseLeave={() => this.removeClass()}>
           <div className="top__item top__item--first">
             <a href='mailto:robert@bob-acken.com'>mail</a>
             <a href='https://www.instagram.com/bobserved'>instagram</a>
             <a href='https://www.facebook.com/robert.o.pettersson'>facebook</a>
           </div>
-          <div className="top__item top__item--second"></div>
+          <div className="top__item top__item--second" id='image'></div>
         </div>
       </div>
     )

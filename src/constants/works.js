b1d0248a23ac574a80e780aca9b1886718eb2537
Robert Pettersson <robert@bob-acken.com>
2018-03-16 15:53:16 -0700

    Adding works to constants, length of works.items is number of boxes in grid.

---
+++ src/constants/works.js
@@ -0,0 +1,26 @@
+import phone from '../assets/2.jpg'
+const WORKS = {
+  items: [
+    { id: 0 },
+    { id: 1, name: 'phone', img: phone },
+    { id: 2 },
+    { id: 3 },
+    { id: 4 },
+    { id: 5 },
+    { id: 6 },
+    { id: 7, name: 'test', img: phone },
+    { id: 8 },
+    { id: 9 },
+    { id: 10 },
+    { id: 11, name: 'phone', img: phone },
+    { id: 12 },
+    { id: 13 },
+    { id: 14, name: 'phone', img: phone },
+    { id: 15 },
+    { id: 16 }
+  ]
+}
+
+export {
+  WORKS
+}

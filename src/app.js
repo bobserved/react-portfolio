80eaad32a844ffd552706b183bd0c3acfd26806c
Robert Pettersson <robert@bob-acken.com>
2018-03-20 14:20:04 -0700

    Adding temporary mobile version.

--- src/app.js
+++ src/app.js
@@ -16,17 +16,22 @@ import './styles/app.css'
 const App = () => {
     return (
       <Router>
-        <div className="app">
-          <header className="app-header">
-            <ul className="app-nav">
-              <li className="app-nav__item"><Link to="/">Home</Link></li>
-              <li className="app-nav__item"><Link to="/work">Work</Link></li>
-              <li className="app-nav__item"><Link to="/about">About</Link></li>
-            </ul>
-          </header>  
-          <Route exact path="/" component={Home}/>
-          <Route path="/about" component={About}/>
-          <Route path="/work" component={Work}/>
+        <div>
+          <div className="app-mobile">
+            <h1 className="app-mobile__heading">Mobile version coming soon!</h1>
+          </div>
+          <div className="app">
+            <header className="app-header">
+              <ul className="app-nav">
+                <li className="app-nav__item"><Link to="/">Home</Link></li>
+                <li className="app-nav__item"><Link to="/work">Work</Link></li>
+                <li className="app-nav__item"><Link to="/about">About</Link></li>
+              </ul>
+            </header>  
+            <Route exact path="/" component={Home}/>
+            <Route path="/about" component={About}/>
+            <Route path="/work" component={Work}/>
+          </div>
         </div>
       </Router>
     )
